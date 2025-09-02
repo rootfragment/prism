b8ac55a145026483f16f7939443137d7e8f8b492
rootfragment <ananthanr.off@gmail.com>
2025-09-02 19:07:01 +0530

    Add files via upload

---
+++ learning_progress/10-hashing_with_buffer/10-hashing_with_buffer.py
@@ -0,0 +1,43 @@
+import hashlib
+import multiprocessing as mp
+import time
+
+def line_hash(line):
+    word = line.strip()
+    return word + ":" + hashlib.md5(word.encode()).hexdigest()
+    
+def process_(lines):
+    return [line_hash(line) for line in lines]
+
+def chunks(file , size):
+    chunk = []
+    for item in file:
+        chunk.append(item)
+        if len(chunk)==size:
+            yield chunk
+            chunk = []
+    if chunk:
+            yield chunk
+
+filename = input("Enter the name of input file : ")
+opfile = input("Enter the name of the output file : ")
+
+start = time.time()
+cpu_count = mp.cpu_count()
+
+buffer = []
+lines_per_write = 10000
+with open(filename, "r", encoding="latin-1", errors="ignore") as f:
+    with open(opfile , "w") as fw:
+        with mp.Pool(cpu_count) as pool:
+            for results in pool.imap(process_,chunks(f,10000)):
+                buffer.extend(results)
+                if (len(buffer) >= lines_per_write):
+                    fw.write("\n".join(results) + "\n")
+                    buffer = []
+        if buffer:
+            fw.write("\n".join(results) + "\n")
+        
+end = time.time()
+print(f"time taken : {end -start:.2f} seconds")
+
